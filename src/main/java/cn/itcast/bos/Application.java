10346d3bf256a5665cc136ec3b3b39d1cd343874
gys <gys13200@163.com>
2017-05-02 07:46:03 +0800

    update pom

--- src/main/java/cn/itcast/bos/Application.java
+++ src/main/java/cn/itcast/bos/Application.java
@@ -2,7 +2,9 @@ package cn.itcast.bos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -15,7 +17,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @ServletComponentScan
 @ComponentScan
 @EnableTransactionManagement
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);

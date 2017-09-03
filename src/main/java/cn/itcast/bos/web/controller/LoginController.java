558e1f187de9aa4a2c501c0db4dd8c5a63ecf354
gys <gys13200@163.com>
2017-09-03 14:09:00 +0800

    repaire bugs

--- src/main/java/cn/itcast/bos/web/controller/LoginController.java
+++ src/main/java/cn/itcast/bos/web/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.UnknownSessionException;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -105,8 +106,11 @@ public class LoginController {
 		Subject subject = SecurityUtils.getSubject();
 		subject.logout();
 
+		try{
+			request.getSession().invalidate();
+		}catch (Exception e){}
 		// 销毁当前Session对象
-		request.getSession().invalidate();
+
 		return "redirect:login.jsp";
 	}
 }
